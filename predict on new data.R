eabf9cc7e60ae763f1db20d5f4d9eba985f9f5a4
MA-QUT <48538028+MA-QUT@users.noreply.github.com>
2020-03-24 15:39:25 +1000

    Add files via upload

--- predict on new data.R
+++ predict on new data.R
@@ -1,23 +1,22 @@
-rm(list=ls())
-
-#' Make predictions on new data
+#' Make predictions on new data with the energy expenditure prediction models trained on free-living data
 # install and load the following packages:
 library(kernlab)
 library(randomForest)
 library(nnet)
+library(RCurl)
 
 
-# Wrist dataset
-wristdf<-fread("C:/Users/mha88/Desktop/QUT/Completed Manuscripts/preschool EE/manuscript/Submission/resubmission 1/Github/new_data wrist.csv",
+# Sample Wrist dataset
+wristdf<-fread(RCurl::getURLContent("https://raw.githubusercontent.com/MA-QUT/Preschool_EE_Models_PLOS_One/master/models/new_data%20wrist.csv"),
                data.table = F)
 
 ############### 1) svm wrist model
-load("C:/Users/mha88/Desktop/QUT/Completed Manuscripts/preschool EE/manuscript/Submission/resubmission 1/Github/wrist_svm.RData")
+load(url("https://github.com/MA-QUT/Preschool_EE_Models_PLOS_One/raw/master/models/wrist_svm.RData"))
 
-a<-rownames(data.frame(wrist_svm$svmmodel@scaling$x.scale))
+featurenames<-rownames(data.frame(wrist_svm$svmmodel@scaling$x.scale))
 
 #Identify the features needed for the wrist SVM model from the dataset
-test_wristsvm<-wristdf[,c(which(colnames(wristdf)%in%a))]
+test_wristsvm<-wristdf[,c(which(colnames(wristdf)%in%featurenames))]
 
 #Normalize feature set values
 test_wristsvm<-scale(test_wristsvm,center = wrist_svm$PreProcess_mean,scale = wrist_svm$PreProcess_std)
@@ -27,51 +26,50 @@ predict_wristsvm<-predict(wrist_svm$svmmodel,test_wristsvm)
 
 
 ############### 2) rf wrist model
-load("C:/Users/mha88/Desktop/QUT/Completed Manuscripts/preschool EE/manuscript/Submission/resubmission 1/Github/wrist_rf.RData")
+load(url("https://github.com/MA-QUT/Preschool_EE_Models_PLOS_One/raw/master/models/wrist_rf.RData"))
 
-a<-wrist_rf$xNames
+featurenames<-wrist_rf$xNames
 
 #Identify the features needed for the wrist RF model from the dataset
-test_wristrf<-wristdf[,c(which(colnames(wristdf)%in%a))]
+test_wristrf<-wristdf[,c(which(colnames(wristdf)%in%featurenames))]
 
 predict_wristrf<-predict(wrist_rf,test_wristrf)
 #################
 
-# hip dataset
-hipdf<-fread("C:/Users/mha88/Desktop/QUT/Completed Manuscripts/preschool EE/manuscript/Submission/resubmission 1/Github/new_data hip.csv",
+
+
+# Samplehip dataset
+hipdf<-fread(RCurl::getURLContent("https://raw.githubusercontent.com/MA-QUT/Preschool_EE_Models_PLOS_One/master/models/new_data%20hip.csv"),
                data.table = F)
 
 ############### 3) ann hip model
-load("C:/Users/mha88/Desktop/QUT/Completed Manuscripts/preschool EE/manuscript/Submission/resubmission 1/Github/hip_ann.RData")
-a<-hip_ann$coefnames
+load(url("https://github.com/MA-QUT/Preschool_EE_Models_PLOS_One/raw/master/models/hip_ann.RData"))
+
+featurenames<-hip_ann$coefnames
 
 #Identify the features needed for the hip ANN model from the dataset
-test_hipann<-hipdf[,c(which(colnames(hipdf)%in%a))]
+test_hipann<-hipdf[,c(which(colnames(hipdf)%in%featurenames))]
 
 predict_hipann<-predict(hip_ann,test_hipann)
 
 
 ############### 4) rf hip model
-load("C:/Users/mha88/Desktop/QUT/Completed Manuscripts/preschool EE/manuscript/Submission/resubmission 1/Github/hip_rf.RData")
+load(url("https://github.com/MA-QUT/Preschool_EE_Models_PLOS_One/raw/master/models/hip_rf.RData"))
 
-a<-hip_rf$xNames
+featurenames<-hip_rf$xNames
 
 #Identify the features needed for the wrist RF model from the dataset
-test_hiprf<-hipdf[,c(which(colnames(hipdf)%in%a))]
+test_hiprf<-hipdf[,c(which(colnames(hipdf)%in%featurenames))]
 
 predict_hiprf<-predict(hip_rf,test_hiprf)
 #################
 
 
-plot(NA,ylim=range(0,6),xlim=range(0,6),ylab="predicted",xlab="observed")
+############## combine the observed and predicted kcals/min
 
-points(wristdf$kcal,predict_wristsvm,pch=15,col='orange')
-points(wristdf$kcal,predict_wristrf,pch=15,col='blue')
-
-points(hipdf$kcal,predict_hipann,pch=16,col='green')
-points(hipdf$kcal,predict_hiprf,pch=16,col='red')
-
-abline(0,1)
-
+kcals_min<-cbind(hipdf$kcal,
+                      predict_wristsvm, predict_wristrf,
+                      predict_hipann, predict_hiprf)
+colnames(kcals_min)<-c("observed","wrist_svm","wrist_rf","hip_ann","hip_rf")
 
 
